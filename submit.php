b127fe3734126bd72ef95a3d79e1e544e383c961
knoiz <johnrey.gimeno@sdca.edu.ph>
2024-09-27 11:00:01 +0800

    2nd commit

--- submit.php
+++ submit.php
@@ -1,11 +1,11 @@
 <?php
 
-$first_name = $_POST ["first_name"];
-$last_name = $_POST["last_name"];
-$email = $_POST["email"];
-$password = $_POST["password"];
-$date = $_POST["date"];
-$yearlvl = $_POST["year-level"];
+$first_name = $_POST ["First_name"];
+$last_name = $_POST["Last_name"];
+$email = $_POST["Email"];
+$password = $_POST["Password"];
+$date = $_POST["Date"];
+$yearlvl = $_POST["Year-level"];
 
 echo "checkbox" . json_encode ($_POST["checkbox"]);
 
